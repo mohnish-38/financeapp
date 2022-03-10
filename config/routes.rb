c31678ebc30de514e95c899890e03eb901bcba7d
mohnish-38 <jharbadeji38@gmail.com>
2022-03-10 16:03:46 +0530

    added user search and add friend functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
 end
